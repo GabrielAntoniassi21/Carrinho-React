cf843d03aa6d9b83e19a87e225c7ccb3e160e2c4
GabrielAntoniassi21 <gabriel.antoniassi21@gmail.com>
2026-09-08 20:41:29 -0300

    adiciona estrutura e produtos do carrinho

---
+++ src/data/produtos.js
@@ -0,0 +1,22 @@
+const produtos = [
+    {
+        id: 1,
+        nome: "Camiseta",
+        preco: 59.90,
+        quantidade: 2
+    },
+    {
+        id: 2,
+        nome: "Calça Jeans",
+        preco: 129.90,
+        quantidade: 1
+    },
+    {
+        id: 3,
+        nome: "Tênis",
+        preco: 199.90,
+        quantidade: 1
+    }
+];
+
+export default produtos;
